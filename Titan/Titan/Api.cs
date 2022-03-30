f7ea9843b39d3f6fa2cf6d708823aa694ffb1c09
LT69Baby <trevor998tredoux@gmail.com>
2022-03-30 15:29:21 +0200

    Titan

---
+++ Titan/Titan/Api.cs
@@ -0,0 +1,81 @@
+﻿namespace Titan
+{
+    public static class Api
+    {
+        public static void ConfigureApi(this WebApplication app)
+        {
+            // All of my API endpoint mapping
+            app.MapGet("/Users", GetUsers);
+            app.MapGet("/Users/{id}", GetUser);
+            app.MapPost("/Users", InsertUser);
+            app.MapPut("/Users", UpdateUser);
+            app.MapDelete("/Users", DeleteUser);
+        }
+
+        private static async Task<IResult> GetUsers(IUser data)
+        {
+            try
+            {
+                return Results.Ok(await data.GetUsers());
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> GetUser(int id, IUser data)
+        {
+            try
+            {
+                var results = await data.GetUser(id);
+                if (results == null) return Results.NotFound();
+                return Results.Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> InsertUser(User user, IUser data)
+        {
+            try
+            {
+                await data.InsertUser(user);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> UpdateUser(User user, IUser data)
+        {
+            try
+            {
+                await data.UpdateUser(user);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> DeleteUser(int id, IUser data)
+        {
+            try
+            {
+                await data.DeleteUser(id);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+    }
+}
